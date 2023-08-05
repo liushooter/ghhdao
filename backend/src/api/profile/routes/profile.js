7bed218d7fd0800b8d54fd1542f5ab6b425ce903
liushooter <shooter.hero@gmail.com>
2023-08-06 00:48:46 +0800

    user has one profile

--- backend/src/api/profile/routes/profile.js
+++ backend/src/api/profile/routes/profile.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * person router
+ * profile router
  */
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::person.person');
+module.exports = createCoreRouter('api::profile.profile');
